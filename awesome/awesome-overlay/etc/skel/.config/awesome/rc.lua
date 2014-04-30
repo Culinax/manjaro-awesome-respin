f075b395770f830acb7dbc8760cf9785bdbc2cc4
Culinax <culinax.d@gmail.com>
2014-04-30 19:46:23 +0200

    Disable startup-notifications globally

--- awesome/awesome-overlay/etc/skel/.config/awesome/rc.lua
+++ awesome/awesome-overlay/etc/skel/.config/awesome/rc.lua
@@ -559,3 +559,10 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- {{{ Disable startup-notification globally
+local oldspawn = awful.util.spawn
+awful.util.spawn = function (s)
+    oldspawn(s, false)
+end
+-- }}}
